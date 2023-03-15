f9001a5ee19b73b37824293c91525c5e05069ec2
muteeb28 <3muteebmasoodi@gmail.com>
2023-03-15 15:13:12 +0530

    modified the landing page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn =document.getElementById("menu-btn");
+// const overlay=document.getElementById("overlay");
+btn.addEventListener("click",navToggle);
+function navToggle(){
+    btn.classList.toggle("open");
+    // overlay.classList.toggle("overlay-show");
+}
